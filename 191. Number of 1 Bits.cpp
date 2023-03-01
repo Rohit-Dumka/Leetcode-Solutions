64e785f56f84450a2aef5ec4d244b33e5c492835
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-03-01 23:43:28 +0530

    Update 191. Number of 1 Bits.cpp

--- 191. Number of 1 Bits.cpp
+++ 191. Number of 1 Bits.cpp
@@ -12,4 +12,15 @@ public:
     }
 };
 
-Approach: 
+Approach :Here given the binary number as an input.
+          every input consisting of 32 bits( 'O's and '1's ).
+
+first we right shift all the 1's until the binary number will be equivalent to 0 or till there is no 1 left.
+    then we count the number of 1's at the same time by using the bit manipulation
+    here we are using and operator i.e. n&1 which gives 1 only when the unit bit is 1 
+    and 0 when the unit is zero and count the numbers of 1's by side.
+    
+    
+
+
+    
