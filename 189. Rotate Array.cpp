bb1abeb3ca04958e80393122c23a028b4db18216
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-03-05 21:51:44 +0530

    Update 189. Rotate Array.cpp

--- 189. Rotate Array.cpp
+++ 189. Rotate Array.cpp
@@ -4,6 +4,7 @@ public:
     void rotate(vector<int>& nums, int k) 
     {
         k %=nums.size();
+        //since every nth time the array is in it's initial position
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin()+k);
         reverse(nums.begin()+k, nums.end());
