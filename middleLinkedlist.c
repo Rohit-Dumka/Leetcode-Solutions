47a763d65268693ec7678c974e9369c906e1531b
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-05-02 11:25:45 -0700

    Update middleLinkedlist.c

--- middleLinkedlist.c
+++ middleLinkedlist.c
@@ -1,1 +1,27 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head){
+    
+    struct ListNode* curr=head;
+    struct ListNode* temp=head;
+        int length=1,i=1;
+        while(curr!=NULL)
+        {
+            curr=curr->next;
+            length++;
+        }
+    
+        if(length%2!=0)
+            length=length+1;
+        
+        for(;i<length/2;i++)
+        {
+            temp=temp->next;
+        }
+    return temp;
+}
