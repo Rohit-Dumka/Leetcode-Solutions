c40cc3e16f931b36b3d3c59b1171974e0ceb3c39
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-05-05 12:27:38 -0700

    Create merge2SortedArray.c

---
+++ Medium/merge2SortedArray.c
@@ -0,0 +1,11 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    int i=m-1,j=n-1,k=nums1Size-1;
+    while(i>=0 && j>=0)
+    {
+        nums1[k--]=(nums2[j]>nums1[i])? nums2[j--] : nums1[i--];
+    }
+    while(j>=0)
+    {
+        nums1[k--]=nums2[j--];
+    }
+}
