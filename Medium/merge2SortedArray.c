33539b02b38ccc4039ef2b46807d721eda8c542a
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-05-06 11:15:07 -0700

    Update merge2SortedArray.c

--- Medium/merge2SortedArray.c
+++ Medium/merge2SortedArray.c
@@ -4,6 +4,7 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
     {
         nums1[k--]=(nums2[j]>nums1[i])? nums2[j--] : nums1[i--];
     }
+    
     while(j>=0)
     {
         nums1[k--]=nums2[j--];
