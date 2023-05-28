f37d23fd6a8957e319566f6a17bc5dfe41091138
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-05-28 11:05:01 -0700

    Added solution - LeetHub

---
+++ Remove characters from alphanumeric string - GFG/remove-characters-from-alphanumeric-string.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            
+            Solution ob = new Solution();
+            String result = ob.removeCharacters(s);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String removeCharacters(String S) {
+        S =S.replaceAll("[^0-9]", "");
+        return S;
+    }
+}
